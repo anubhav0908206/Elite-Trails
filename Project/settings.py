01fc2ee5fcfb6e18d05a6af2693be1fac14fa212
anubhav0908206 <anubhavgaurav09@gmail.com>
2026-05-02 05:11:45 +0530

    Fix static files and allowed hosts

--- Project/settings.py
+++ Project/settings.py
@@ -120,7 +120,7 @@ import os
 STATIC_URL = 'Static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'Static')]
 
-STATIC_ROOTS = [os.path.join(BASE_DIR,'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
